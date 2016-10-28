278351ebcdf6060be7e13d66455bc12d44d0b70c
Michiel ter Reehorst <michiel@swydo.com>
2016-10-28 15:36:46 +0200

    0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'swydo:ddp-apollo',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'DDP network interface for Apollo using a Meteor method',
   git: 'https://github.com/swydo/ddp-apollo',
   documentation: 'README.md'
