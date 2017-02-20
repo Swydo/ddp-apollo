dbf758853ba47833bb628b3af8c30d6f807eab81
Michiel ter Reehorst <michiel@swydo.com>
2017-02-20 21:36:03 +0100

    Run a first basic test

--- package.js
+++ package.js
@@ -1,3 +1,8 @@
+var packages = [
+  'ecmascript',
+  'promise',
+];
+
 Package.describe({
   name: 'swydo:ddp-apollo',
   version: '0.0.6',
@@ -8,14 +13,18 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.2.4');
-  api.use(['ecmascript', 'promise']);
+  api.use(packages);
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
 });
 
-// Package.onTest(function(api) {
-//   api.use('ecmascript');
-//   api.use('tinytest');
-//   api.use('swydo:ddp-apollo');
-//   api.mainModule('ddp-apollo-tests.js');
-// });
+Package.onTest(function(api) {
+  api.use(packages);
+
+  api.use([
+    'practicalmeteor:mocha',
+    'dispatch:mocha-phantomjs',
+  ]);
+
+  api.mainModule('server.spec.js', 'server');
+});
