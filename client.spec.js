d7a454afedf91f77fab14813984c874ae6bca91c
Michiel ter Reehorst <michiel@swydo.com>
2017-02-21 09:42:46 +0100

    Add basic client tests

---
+++ client.spec.js
@@ -0,0 +1,1 @@
+import './lib/ddp-network-interface.spec';
--- package.js
+++ package.js
@@ -8,23 +8,25 @@ Package.describe({
   version: '0.0.6',
   summary: 'DDP network interface for Apollo using a Meteor method',
   git: 'https://github.com/swydo/ddp-apollo',
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
+Package.onUse(function use(api) {
   api.versionsFrom('1.3.2.4');
   api.use(packages);
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function test(api) {
   api.use(packages);
 
   api.use([
     'practicalmeteor:mocha',
+    'practicalmeteor:chai',
     'dispatch:mocha-phantomjs',
   ]);
 
+  api.mainModule('client.spec.js', 'client');
   api.mainModule('server.spec.js', 'server');
 });
