e74e373149827062ada45a48d616deac27d15c24
Michiel ter Reehorst <michiel@swydo.com>
2017-03-13 10:15:39 +0100

    Fix tracing error logs

--- specs/optics.spec.js
+++ specs/optics.spec.js
@@ -12,6 +12,10 @@ describe('Optics', function () {
       chai.expect(context.startWallTime).to.be.a('number');
       chai.expect(context.queries).to.be.an.instanceof(Map);
       chai.expect(context.agent).to.be.an.instanceof(Agent);
+
+      // To make tracing work:
+      chai.expect(context.req).to.be.an('object');
+      chai.expect(context.req.connection).to.be.an('object');
     });
   });
 
