005d11e3eabc5d09860fe5fc42ee02700c4b2e8a
Michiel ter Reehorst <michiel@swydo.com>
2017-11-13 22:41:00 +0100

    1.0.0-beta.5

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ var packages = [
 
 Package.describe({
   name: 'swydo:ddp-apollo',
-  version: '1.0.0-beta.4',
+  version: '1.0.0-beta.5',
   summary: 'DDP link and server for Apollo',
   git: 'https://github.com/swydo/ddp-apollo',
   documentation: 'README.md',
