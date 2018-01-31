8411a12093aead0d13fc06d63623d6993d0ee2aa
Michiel ter Reehorst <michiel@swydo.com>
2018-01-31 13:33:54 +0100

    Remove everything related to Optics
    because as of 2018-01-31 it’s no longer operational

--- specs/server.spec.js
+++ specs/server.spec.js
@@ -1,3 +1,2 @@
 import './server/setup.spec';
-import './server/optics.spec';
 import './server/helpers';
--- specs/server/setup.spec.js
+++ specs/server/setup.spec.js
@@ -1,23 +1,16 @@
 /* eslint-disable prefer-arrow-callback, func-names */
 /* eslint-env mocha */
 import chai from 'chai';
-import sinon from 'sinon';
 import { makeExecutableSchema } from 'graphql-tools';
-import OpticsAgent from 'optics-agent';
 import gql from 'graphql-tag';
 
 import { setup, DDP_APOLLO_SCHEMA_REQUIRED } from '../../lib/server/setup';
 import { createGraphQLMethod } from '../../lib/server/createGraphQLMethod';
 import { DEFAULT_METHOD } from '../../lib/common/defaults';
-import * as optics from '../../lib/server/optics';
 
 import { typeDefs } from '../data/typeDefs';
 import { resolvers } from '../data/resolvers';
 import { reset } from './helpers';
-
-OpticsAgent.configureAgent({
-  apiKey: process.env.OPTICS_API_KEY || 'foo',
-});
 
 describe('#setup', function () {
   beforeEach(function () {
@@ -196,42 +189,6 @@ describe('#setup', function () {
           done();
         })
         .catch(done);
-    });
-  });
-
-  describe('optics', function () {
-    it('should auto-detect if optics should be added', function (done) {
-      const schema = makeExecutableSchema({
-        resolvers,
-        typeDefs,
-      });
-
-      OpticsAgent.instrumentSchema(schema);
-
-      const functions = Object.keys(optics);
-
-      const fakeOptics = functions.reduce((all, current) => {
-        // eslint-disable-next-line no-param-reassign
-        all[current] = function () {
-          // empty
-        };
-        return all;
-      }, {});
-
-      const spies = functions.map(name => sinon.spy(fakeOptics, name));
-
-      createGraphQLMethod(schema, { optics: fakeOptics })();
-
-      Meteor.defer(() => {
-        try {
-          spies.forEach((spy) => {
-            chai.expect(spy.callCount, spy.displayName).to.equal(1);
-          });
-          done();
-        } catch (e) {
-          done(e);
-        }
-      });
     });
   });
 });
