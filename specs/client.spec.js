527210e318dbfc6ce200161e7b08d5a642c4cf95
Michiel ter Reehorst <jamiter@users.noreply.github.com>
2018-06-06 13:10:08 +0200

    fix(graphiql): fix graphiql fetcher authorization (#192)
    * fix(graphiql): fix graphiql fetcher authorization
    
    * Test graphQLFetcher
    
    to prevent issues in the future, especially with authentication

--- specs/client.spec.js
+++ specs/client.spec.js
@@ -3,3 +3,4 @@ import './client/apollo-link-ddp.spec';
 import './client/apollo-client.spec';
 import './client/apollo-link-meteor-auth.spec';
 import './client/apollo-link-meteor.spec';
+import './client/graphQLFetcher.spec';
