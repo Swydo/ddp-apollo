856d050c238e6e6d7a79d77766d7e68d56540dce
Michiel ter Reehorst <michiel@swydo.com>
2018-06-06 12:51:03 +0200

    Test graphQLFetcher
    to prevent issues in the future, especially with authentication

---
+++ specs/client/graphQLFetcher.spec.js
@@ -0,0 +1,47 @@
+/* eslint-disable prefer-arrow-callback, func-names */
+/* eslint-env mocha */
+import chai from 'chai';
+import { createFetcher } from '../../lib/client/graphQLFetcher';
+import { callPromise } from './helpers/callPromise';
+import { loginWithUserId } from './helpers/login';
+
+describe('graphQLFetcher', function () {
+  beforeEach(function () {
+    this.fetcher = createFetcher();
+  });
+
+  it('should return data from the server', async function () {
+    const operation = {
+      query: 'query { foo }',
+    };
+
+    const { data } = await this.fetcher(operation);
+
+    chai.expect(data).to.deep.equal({ foo: 'bar' });
+  });
+
+  describe('when authenticated', function () {
+    before(async function () {
+      const userId = await callPromise('createTestUser');
+      chai.expect(userId).to.be.a('string');
+      this.userId = userId;
+      await loginWithUserId(userId);
+    });
+
+    after(function (done) {
+      Meteor.logout((err) => { err ? done(err) : done(); });
+    });
+
+    it('returns the userId', async function () {
+      const operation = {
+        query: 'query { userId, meteorUserId }',
+      };
+
+      const { data } = await this.fetcher(operation);
+
+      chai.expect(data.userId).to.equal(this.userId);
+      chai.expect(data.meteorUserId).to.equal(this.userId);
+    });
+  });
+});
+
