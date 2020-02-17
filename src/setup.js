a14028482f1bb5d02edb9c1def3a01c65618792a
Michiel ter Reehorst <michiel@swydo.com>
2020-02-17 11:00:13 +0100

    Move setupHttpEndpoint to own file

--- src/setup.js
+++ src/setup.js
@@ -1,15 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import { WebApp } from 'meteor/webapp';
 import {
   DEFAULT_METHOD,
-  DEFAULT_PATH,
 } from 'apollo-link-ddp';
 import { initSchema } from './initSchema';
 import { createExecutor } from './createExecutor';
 import { createGraphQLMethod } from './createGraphQLMethod';
 import { createGraphQLPublication } from './createGraphQLPublication';
-import { createGraphQLMiddleware } from './createGraphQLMiddleware';
-import { meteorAuthMiddleware } from './meteorAuthMiddleware';
+import { setupHttpEndpoint } from './setupHttpEndpoint';
 
 export async function setup({
   schema,
@@ -43,50 +40,7 @@ export async function setup({
   }
 }
 
-export async function setupHttpEndpoint({
-  schema,
-  gateway,
-  path = DEFAULT_PATH,
-  context,
-  engine,
-  jsonParser,
-  authMiddleware = meteorAuthMiddleware,
-} = {}) {
-  const {
-    schema: initializedSchema,
-    executor: gatewayExecutor,
-  } = await initSchema({
-    schema,
-    gateway,
-  });
-
-  const graphQLMiddleware = createGraphQLMiddleware({
-    schema: initializedSchema,
-    context,
-    execute: createExecutor(gatewayExecutor),
-  });
-
-  if (engine && engine.expressMiddleware) {
-    WebApp.connectHandlers.use(engine.expressMiddleware());
-  }
-
-  if (!jsonParser) {
-    // Only require the body-parser for users who actually use the http version
-    // eslint-disable-next-line global-require
-    const bodyParser = require('body-parser');
-    // eslint-disable-next-line no-param-reassign
-    jsonParser = bodyParser.json();
-  }
-
-  WebApp.connectHandlers.use(path, jsonParser);
-
-  if (authMiddleware) {
-    WebApp.connectHandlers.use(path, authMiddleware);
-  }
-
-  WebApp.connectHandlers.use(path, (req, res, next) => graphQLMiddleware(req, res).catch(next));
-}
-
 export {
   createGraphQLPublication,
+  setupHttpEndpoint,
 };
---
+++ src/setupHttpEndpoint.js
@@ -0,0 +1,57 @@
+import { WebApp } from 'meteor/webapp';
+import {
+  DEFAULT_PATH,
+} from 'apollo-link-ddp';
+import { initSchema } from './initSchema';
+import { createExecutor } from './createExecutor';
+import { createGraphQLPublication } from './createGraphQLPublication';
+import { createGraphQLMiddleware } from './createGraphQLMiddleware';
+import { meteorAuthMiddleware } from './meteorAuthMiddleware';
+
+export async function setupHttpEndpoint({
+  schema,
+  gateway,
+  path = DEFAULT_PATH,
+  context,
+  engine,
+  jsonParser,
+  authMiddleware = meteorAuthMiddleware,
+} = {}) {
+  const {
+    schema: initializedSchema,
+    executor: gatewayExecutor,
+  } = await initSchema({
+    schema,
+    gateway,
+  });
+
+  const graphQLMiddleware = createGraphQLMiddleware({
+    schema: initializedSchema,
+    context,
+    execute: createExecutor(gatewayExecutor),
+  });
+
+  if (engine && engine.expressMiddleware) {
+    WebApp.connectHandlers.use(engine.expressMiddleware());
+  }
+
+  if (!jsonParser) {
+    // Only require the body-parser for users who actually use the http version
+    // eslint-disable-next-line global-require
+    const bodyParser = require('body-parser');
+    // eslint-disable-next-line no-param-reassign
+    jsonParser = bodyParser.json();
+  }
+
+  WebApp.connectHandlers.use(path, jsonParser);
+
+  if (authMiddleware) {
+    WebApp.connectHandlers.use(path, authMiddleware);
+  }
+
+  WebApp.connectHandlers.use(path, (req, res, next) => graphQLMiddleware(req, res).catch(next));
+}
+
+export {
+  createGraphQLPublication,
+};
