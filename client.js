7eda57dbe49042f0f39f7e9e041519c0187a696f
Kevin <kevin@swydo.com>
2022-02-18 11:33:27 +0100

    fix: import from @swydo/scoped package

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
-import { DDPLink } from 'apollo-link-ddp';
+import { DDPLink } from '@swydo/apollo-link-ddp';
 
-export * from 'apollo-link-ddp';
+export * from '@swydo/apollo-link-ddp';
 
 // It is common for Apollo Links to export the link itself as the default
 export default DDPLink;
